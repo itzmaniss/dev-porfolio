361eac4c81e209d8889671f0ee97d1f4b932662c
Manish <87887011+itzmaniss@users.noreply.github.com>
2024-06-14 07:21:40 +0000

    Completed HTMX and CSS for projects

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Elysia, t } from "elysia";
 import { staticPlugin } from "@elysiajs/static"
-import Database from "better-sqlite3"
+import { Database } from "bun:sqlite"
 
 interface Project {
   name: string;
   desc: string;
+  repo: string;
 }
 
 const app = new Elysia()
@@ -14,21 +15,26 @@ const app = new Elysia()
 .get("/", () => Bun.file("./src/index.html"))
 
 // Get the projects i want to display on my website from a db
-.post("/projects", async () => {
-  const db = new Database("my_projects.db");
-  const projects = db.prepare("SELECT name, desc FROM projects WHERE display = 1").all() as Project[];
+.post("/projects", () => {
+  const db = new Database("./public/my_projects.db");
+  const projects = db.prepare("SELECT name, desc, repo FROM projects WHERE display = 1").all() as Project[];
   db.close();
 
-  const project_list = projects.map(project => `
-    <li>
-    ${project.name}
-    </li>
+  const project_list = projects.map((project, index) => `
+    <div class="project ${ index % 2 === 0 ? "even" : "odd"}">
+      <img src= "./public/${ escapehtml(project.name) }.jpg" alt= "${ escapehtml(project.name) } image" class="project-image">
+      <div class="project-content">
+        <a href= ${ escapehtml(project.repo) } class="project-name" target="_blank">${ escapehtml(project.name) } </a>
+        <p class="project-desc"> ${ escapehtml(project.desc) } </p>
+      </div>
+    </div>
     `).join(" ");
+  return project_list;
 })
 
 // Downloads my resume
 .post( "/resume",  async ( {set} ) => {
-  const pdf_file = Bun.file("./public/manish_resume.pdf");
+  const pdf_file = Bun.file("./public/my_resume.pdf");
 
   set.headers["Content-Disposition"] = "attachment; filename=manish_resume.pdf";
 
@@ -39,6 +45,15 @@ const app = new Elysia()
   response: t.File()
 }) 
 .listen(3000);
+
+function escapehtml(text: string){
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
